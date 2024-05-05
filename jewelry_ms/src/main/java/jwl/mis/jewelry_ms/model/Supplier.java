8c47537a4966d2ae20a51f7b35e4d47b16aeda60
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-05-05 10:34:57 +0530

    entity

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Supplier.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Supplier.java
@@ -16,12 +16,15 @@ public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long seller_id;
-    private String supname;
-    private String address;
-    private String itemid;
-    private String quantity;
+    private String firstname;
+    private String lastname;
     private String phonenumber;
     private String email;
+    private String password;
+    private String address;
+    private String dob;
+
+
 
 
 }
