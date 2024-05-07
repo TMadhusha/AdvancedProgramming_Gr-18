4393bc73ffc498822653b2113aae4f1052863be6
Tirasha <1999tirasha@gmail.com>
2024-05-07 23:53:08 +0530

    last

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/InventoryRepository.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/InventoryRepository.java
@@ -3,5 +3,5 @@ package jwl.mis.jewelry_ms.repository;
 import jwl.mis.jewelry_ms.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+public interface InventoryRepository extends JpaRepository<Inventory,String> {
 }
