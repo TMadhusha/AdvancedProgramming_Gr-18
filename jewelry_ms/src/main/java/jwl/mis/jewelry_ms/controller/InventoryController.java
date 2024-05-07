262d0b37fdbe24f453cd3dde32eb5962b755c383
Tirasha <1999tirasha@gmail.com>
2024-05-07 22:55:37 +0530

    Update InventoryController.java

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/InventoryController.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/InventoryController.java
@@ -22,33 +22,5 @@ public class InventoryController {
         return inventoryRepository.findAll();
     }
 
-    @GetMapping("/inventory/{item_id}")
-    Inventory getInventoryByItemId(@PathVariable String item_id){
-        return inventoryRepository.findById(item_id)
-                .orElseThrow(()->new InventoryNotFoundException(item_id));
-    }
-
-    @PutMapping("/inventory/{item_id}")
-    Inventory updateInventory(@RequestBody Inventory newInventory,@PathVariable String item_id){
-        System.out.println("test");
-        return inventoryRepository.findById(item_id)
-                .map(inventory -> {
-                    inventory.setName(newInventory.getName());
-                    inventory.setDescription(newInventory.getDescription());
-                    inventory.setStartingPrice(newInventory.getStartingPrice());
-                    inventory.setImage(newInventory.getImage());
-
-                    return inventoryRepository.save(inventory);
-                }).orElseThrow(()->new InventoryNotFoundException(item_id));
-    }
-
-    @DeleteMapping("/inventory/{item_id}")
-    String deleteInventory(@PathVariable String item_id){
-        if(!inventoryRepository.existsById(item_id)){
-            throw new InventoryNotFoundException(item_id);
-        }
-        inventoryRepository.deleteById(item_id);
-        return "Product with ID "+item_id+ " has been deleted successfully";
-    }
-
+   
 }
