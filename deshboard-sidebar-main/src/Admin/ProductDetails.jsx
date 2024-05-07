d883544c17336983d8f98638099543ab2317fd97
TMadhusha <madhusha200069@gmail.com>
2024-05-07 22:50:06 +0530

    last

--- deshboard-sidebar-main/src/Admin/ProductDetails.jsx
+++ deshboard-sidebar-main/src/Admin/ProductDetails.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Adminbar from '../components/Adminbar'
+import axios from 'axios';
 
 export default function ProductDetails() {
-    const [products,setProducts]=useState(false);
-    
+    const [inventory,setInventory]=useState([]);
+
+    useEffect(()=>{
+        loadProduct();
+    },[])
+
+    const loadProduct=async()=>{
+        const result=await axios.get("http://localhost:8080/inventory");
+        setInventory(result.data);
+    }
+
   return (
     <div>
         <Adminbar/>
         <div>
             <div>
                 <h1>Product Details</h1>
+            </div>
+            <div>
+                <button>Add New Product</button>
             </div>
             <div>
             <table class="table">
@@ -25,12 +38,22 @@ export default function ProductDetails() {
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+    {
+        inventory.map((inventory,index)=>(
+            <tr key={index}>
+                <td>{inventory.pro_id}</td>
+                <td><img src={`data:image/jpeg;base64,${inventory.image}`} alt="Product"/></td>
+                <td>{inventory.name}</td>
+                <td>{inventory.author}</td>
+                <td>{inventory.description}</td>
+                <td>{inventory.startingPrice}</td>
+                <td><button>Update</button></td>
+                <td><button>Delete</button></td>
+            </tr>
+        ))
+    }
+    
+    
   </tbody>
 </table>
             </div>
--- deshboard-sidebar-main/src/pages/Product.jsx
+++ deshboard-sidebar-main/src/pages/Product.jsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../components/Sidebar'
+import axios from 'axios';
 
 export default function Product() {
+  const [inventory,setInventory]=useState([]);
+
+    useEffect(()=>{
+        loadProduct();
+    },[])
+
+    const loadProduct=async()=>{
+        const result=await axios.get("http://localhost:8080/inventory");
+        setInventory(result.data);
+    }
   return (
     <div>
         <div>
             <Sidebar/>
         </div>
         <div>
-          Product List
+            <div>
+                <h1>Product Details</h1>
+            </div>
+            <div>
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Author</th>
+      <th scope="col">Description</th>
+      <th scope="col">Starting Price </th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        inventory.map((inventory,index)=>(
+            <tr key={index}>
+                <td>{inventory.pro_id}</td>
+                <td><img src={`data:image/jpeg;base64,${inventory.image}`} alt="Product"/></td>
+                <td>{inventory.name}</td>
+                <td>{inventory.author}</td>
+                <td>{inventory.description}</td>
+                <td>{inventory.startingPrice}</td>
+                <td><button>BID</button></td>
+            </tr>
+        ))
+    }
+    
+    
+  </tbody>
+</table>
+            </div>
         </div>
     </div>
   )
